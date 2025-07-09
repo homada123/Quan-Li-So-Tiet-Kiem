534a7b77a6711e42c21bb2029363798b9507cf9f
DangSinh <94754027+DangSinh@users.noreply.github.com>
2025-07-09 14:25:28 +0700

    Add files via upload

---
+++ config/cnStr.js
@@ -0,0 +1,9 @@
+module.exports = {
+    host: 'localhost',
+    port: 5432,
+    database: 'postgres',
+    user: 'postgres',
+    password: '123456', // ← thay đúng mật khẩu bạn đã đặt
+    max: 30
+  };
+  
